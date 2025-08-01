7a7471cbc01442fe091f5d312a5aa39b7cd8b987
adamlaycock <adam.m.laycock@gmail.com>
2025-08-01 18:36:12 +0100

    functionalise construct table

--- vigenère_cipher.py
+++ vigenère_cipher.py
@@ -1,25 +1,27 @@
 import pandas as pd
 
 ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+table_key = 'hello123'
 
 def rotate_left(alphabet_list: list):
     return alphabet_list[1:] + alphabet_list[:1]
 
-table_key = 'hello123'
-table_key = list(table_key.upper())
+def construct_table(table_key: str):
+    table_key = list(table_key.upper())
 
-alphabet = list(set(table_key + [char for char in ALPHABET if char not in table_key]))
+    alphabet = list(set(table_key + [char for char in ALPHABET if char not in table_key]))
 
-table = []
-for i in range(len(alphabet) - 1):
-    if i != 0:
-        alphabet = rotate_left(alphabet)
-        table.append(alphabet)
-    else:
-        table.append(alphabet)
+    table = []
+    for i in range(len(alphabet)):
+        if i != 0:
+            alphabet = rotate_left(alphabet)
+            table.append(alphabet)
+        else:
+            table.append(alphabet)
 
-table = pd.DataFrame(table)
-table.columns = ALPHABET
-table.index = ALPHABET
+    table = pd.DataFrame(table)
+    table.columns = ALPHABET
+    table.index = ALPHABET
 
+    return table
 

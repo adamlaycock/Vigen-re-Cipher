96c432eed2e7fd1633c12270a2e0260479620345
adamlaycock <adam.m.laycock@gmail.com>
2025-08-01 21:01:20 +0100

    refactor all functions

--- vigenère_cipher.py
+++ vigenère_cipher.py
@@ -2,26 +2,34 @@ import pandas as pd
 
 ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
-def construct_table(table_key: str):
+def construct_table(
+    table_key: str
+) -> pd.DataFrame:
+    
     table_key = list(table_key.upper())
     seen = set()
-    alphabet = [c for c in table_key + ALPHABET if not (c in seen or seen.add(c))]
+    alphabet = [char for char in table_key + ALPHABET if not (char in seen or seen.add(char))]
 
     table = []
     for i in range(len(alphabet)):
         rotated = alphabet[i:] + alphabet[:i]
         table.append(rotated)
 
-    df = pd.DataFrame(table)
-    df.columns = alphabet
-    df.index = alphabet
-    return df
+    table = pd.DataFrame(table)
+    table.columns = alphabet
+    table.index = alphabet
+
+    return table
+
 
 def encrypt_plaintext(
     plaintext: str,
     key: str,
-    table: pd.DataFrame
-) -> str:
+    table_key: str
+):
+    
+    table = construct_table(table_key)
+
     plaintext = plaintext.upper()
     key = key.upper()
     ext_key = (key * ((len(plaintext) // len(key)) + 1))[:len(plaintext)]
@@ -29,13 +37,17 @@ def encrypt_plaintext(
     cipher_text = []
     for p_char, k_char in zip(plaintext, ext_key):
         cipher_text.append(table.loc[p_char, k_char])
-    return ''.join(cipher_text)
+
+    print(''.join(cipher_text))
+
 
 def decrypt_plaintext(
     cipher_text: str,
     key: str,
-    table: pd.DataFrame
+    table_key: str
 ):
+    table = construct_table(table_key)
+
     cipher_text = cipher_text.upper()
     key = key.upper()
     ext_key = (key * ((len(cipher_text) // len(key)) + 1))[:len(cipher_text)]
@@ -43,4 +55,5 @@ def decrypt_plaintext(
     plaintext = []
     for c_char, k_char in zip(cipher_text, ext_key):
         plaintext.append(table[table[k_char] == c_char].index[0])
-    return ''.join(plaintext)
+
+    print(''.join(plaintext))

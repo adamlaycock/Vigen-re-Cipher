b3476da756ca26f5547cb2acf755d4a329108643
adamlaycock <adam.m.laycock@gmail.com>
2025-08-01 17:11:40 +0100

    add custom alphabet

--- vigenère_cipher.py
+++ vigenère_cipher.py
@@ -1,11 +1,20 @@
-table_key = list('hello')
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+
+
 
 def rotate_left(string: str):
     return string[1:] + string[:1]
 
+table_key = 'hello123'
 
-def build_table(
-    table_key: str,
-):
-    table = []
+alphabet = list(ALPHABET)
+table_key = list(table_key.upper())
+
+custom_alphabet = table_key + [char for char in alphabet if char not in table_key]
+
+print(custom_alphabet)
+
+
+        
 

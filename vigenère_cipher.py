31982e80a15dd96c5543ee6254b96137f309d8fd
adamlaycock <adam.m.laycock@gmail.com>
2025-08-01 18:59:02 +0100

    add encrypt_plaintext

--- vigenère_cipher.py
+++ vigenère_cipher.py
@@ -2,26 +2,38 @@ import pandas as pd
 
 ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 table_key = 'hello123'
-
-def rotate_left(alphabet_list: list):
-    return alphabet_list[1:] + alphabet_list[:1]
+key = 'lucifer'
+plaintext = 'thisisasecretmessage'
 
 def construct_table(table_key: str):
     table_key = list(table_key.upper())
-
-    alphabet = list(set(table_key + [char for char in ALPHABET if char not in table_key]))
+    seen = set()
+    alphabet = [c for c in table_key + ALPHABET if not (c in seen or seen.add(c))]
 
     table = []
     for i in range(len(alphabet)):
-        if i != 0:
-            alphabet = rotate_left(alphabet)
-            table.append(alphabet)
-        else:
-            table.append(alphabet)
+        rotated = alphabet[i:] + alphabet[:i]
+        table.append(rotated)
 
-    table = pd.DataFrame(table)
-    table.columns = ALPHABET
-    table.index = ALPHABET
+    df = pd.DataFrame(table)
+    df.columns = alphabet
+    df.index = alphabet
+    return df
 
-    return table
+def encrypt_plaintext(
+    plaintext: str,
+    key: str,
+    table: pd.DataFrame
+) -> str:
+    plaintext = plaintext.upper()
+    key = key.upper()
+    ext_key = (key * ((len(plaintext) // len(key)) + 1))[:len(plaintext)]
+    
+    cipher_text = []
+    for p_char, k_char in zip(plaintext, ext_key):
+        cipher_text.append(table.loc[p_char, k_char])
+    return ''.join(cipher_text)
 
+table = construct_table(table_key)
+encrypted = encrypt_plaintext(plaintext, key, table)
+print(encrypted)
